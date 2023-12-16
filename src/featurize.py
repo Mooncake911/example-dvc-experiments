c3743137fcf648daf9f43397496febb2f4f95ba8
Vadim Shaidurov <shaidurovvadik@yandex.ru>
2023-12-16 14:38:13 +0300

    f

--- src/featurize.py
+++ src/featurize.py
@@ -3,8 +3,6 @@ import pandas as pd
 from typing import Text
 import yaml
 
-from src.utils.logs import get_logger
-
 
 def featurize(config_path: Text) -> None:
     """Create new features.
@@ -15,12 +13,8 @@ def featurize(config_path: Text) -> None:
     with open(config_path) as conf_file:
         config = yaml.safe_load(conf_file)
 
-    logger = get_logger('FEATURIZE', log_level=config['base']['log_level'])
-
-    logger.info('Load raw data')
     dataset = pd.read_csv(config['data_load']['dataset_csv'])
 
-    logger.info('Extract features')
     dataset['sepal_length_to_sepal_width'] = dataset['sepal_length'] / dataset['sepal_width']
     dataset['petal_length_to_petal_width'] = dataset['petal_length'] / dataset['petal_width']
     featured_dataset = dataset[[
@@ -29,7 +23,6 @@ def featurize(config_path: Text) -> None:
         'target'
     ]]
 
-    logger.info('Save features')
     features_path = config['featurize']['features_path']
     featured_dataset.to_csv(features_path, index=False)
 
