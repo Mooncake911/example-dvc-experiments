efe33abb68adab411cedfe6d8b6942da43875c45
Vadim Shaidurov <shaidurovvadik@yandex.ru>
2023-12-16 17:49:49 +0300

    yeah

--- src/featurize.py
+++ src/featurize.py
@@ -1,5 +1,6 @@
 import argparse
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 from typing import Text
 import yaml
 
@@ -14,14 +15,14 @@ def featurize(config_path: Text) -> None:
         config = yaml.safe_load(conf_file)
 
     dataset = pd.read_csv(config['data_load']['dataset_csv'])
+    target_column = config['featurize']['target_column']
 
     dataset['sepal_length_to_sepal_width'] = dataset['sepal_length'] / dataset['sepal_width']
     dataset['petal_length_to_petal_width'] = dataset['petal_length'] / dataset['petal_width']
-    featured_dataset = dataset[[
-        'sepal_length', 'sepal_width', 'petal_length', 'petal_width',
-        'sepal_length_to_sepal_width', 'petal_length_to_petal_width',
-        'target'
-    ]]
+
+    scaler = StandardScaler()
+    featured_dataset = pd.DataFrame(scaler.fit_transform(dataset), columns=dataset.columns)
+    featured_dataset[target_column] = dataset[target_column]
 
     features_path = config['featurize']['features_path']
     featured_dataset.to_csv(features_path, index=False)
---
+++ src/train.py
@@ -0,0 +1,102 @@
+import os
+import argparse
+import pandas as pd
+from typing import Text
+import yaml
+
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+
+from dvclive import Live
+
+
+def draw_confusion_matrix(y_test, y_pred):
+    cm = confusion_matrix(y_test, y_pred)
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", cbar=False)
+    plt.xlabel('Predicted')
+    plt.ylabel('True')
+    plt.title('Confusion Matrix')
+    plt.savefig('plots/confusion_matrix.png')
+
+
+def get_model():
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(3)
+    ])
+
+    loss = tf.losses.SparseCategoricalCrossentropy(from_logits=True)
+    metrics = [tf.keras.metrics.CategoricalAccuracy(name="acc")]
+    optimizer = tf.optimizers.SGD(learning_rate=0.01)
+
+    model.compile(
+        optimizer=optimizer,
+        loss=loss,
+        metrics=metrics,
+    )
+
+    return model
+
+
+def train(config_path: Text) -> None:
+    """Train model.
+    Args:
+        config_path {Text}: path to config
+    """
+
+    with open(config_path) as conf_file:
+        config = yaml.safe_load(conf_file)
+
+    # Определяем модель
+    m = get_model()
+
+    # Подготавливаем данные
+    train_df = pd.read_csv(config['featurize']['features_path'])
+    target_column = config['featurize']['target_column']
+
+    x_train, x_test, y_train, y_test = train_test_split(
+        train_df, train_df[target_column],
+        test_size=config['data_split']['test_size'],
+        random_state=config['base']['random_state']
+    )
+
+    batch_size = config['train']['batch_size']
+    epochs = config['train']['epochs']
+
+    # Вычисляем модель
+    m.fit(
+        x_train,
+        y_train,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=1,
+        validation_data=(x_test, y_test),
+    )
+
+    # Вывод
+    y_prob = m.predict(x_test)
+    y_pred = y_prob.argmax(axis=-1)
+
+    df = pd.DataFrame({
+        'actual': y_test,
+        'predicted': y_pred,
+    })
+    os.makedirs("plots")
+    df.to_csv('plots/confusion.csv', index=False)
+    draw_confusion_matrix(y_test, y_pred)
+
+
+if __name__ == '__main__':
+    live = Live()
+
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument('--config', dest='config', required=True)
+    args = args_parser.parse_args()
+
+    train(config_path=args.config)
